6352b9bf175e7614f6d96968d60e96a30fe47582
palanivelusundarrajan3621 <palanivelusundarrajan3621@gmail.com>
2026-03-01 04:43:49 +0530

    Updated for Railway deployment

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'studentdb'
+import os
+
+app.config['MYSQL_HOST'] = os.environ.get("MYSQLHOST")
+app.config['MYSQL_USER'] = os.environ.get("MYSQLUSER")
+app.config['MYSQL_PASSWORD'] = os.environ.get("MYSQLPASSWORD")
+app.config['MYSQL_DB'] = os.environ.get("MYSQLDATABASE")
+app.config['MYSQL_PORT'] = int(os.environ.get("MYSQLPORT", 3306))
 
 mysql = MySQL(app)
 
@@ -105,5 +108,7 @@ def delete_student(id):
     return jsonify({"message": "Deleted Successfully"})
 
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
